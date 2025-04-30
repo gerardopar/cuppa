b7f287993df66f716f142c0be7c0f28eb6a45431
Gerardo Paredes <gerardparedes23@gmail.com>
2025-04-29 22:20:49 -0700

    set daily trends in cache

--- server/src/controllers/trends.ts
+++ server/src/controllers/trends.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
+import redisClient from "../cache/redis/redisClient";
+
 import { fetchDailyTrends } from "../services/serpApi-service";
 
 export const getTrends = async (req: Request, res: Response): Promise<void> => {
   try {
-    const trends = await fetchDailyTrends();
+    const cacheKey = "trends:top25";
+
+    const cached = await redisClient.get(cacheKey);
+    if (cached) {
+      const trends = JSON.parse(cached) as string[];
+      res.json({ trends });
+    }
+
+    const allTrends = await fetchDailyTrends();
+    const trends = allTrends.slice(0, 25);
+
+    await redisClient.setEx(cacheKey, 86400, JSON.stringify(trends));
 
     res.json({ trends });
   } catch (error) {
