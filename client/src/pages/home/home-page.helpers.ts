c1b0265ea19b427a4dc5045a76d1baa191fa7289
Gerardo Paredes <gerardparedes23@gmail.com>
2025-04-30 23:19:26 -0700

    add news logo assets

--- client/src/pages/home/home-page.helpers.ts
+++ client/src/pages/home/home-page.helpers.ts
@@ -1,3 +1,15 @@
+import CBSNewsLogo from "../../assets/images/cbs-logo.webp";
+import AxiosLogo from "../../assets/images/axios-logo.webp";
+import CNNLogo from "../../assets/images/cnn-logo.png";
+import FoxNewsLogo from "../../assets/images/fox-logo.webp";
+import NBCNewsLogo from "../../assets/images/nbc-logo.png";
+import USATodayLogo from "../../assets/images/usa-today-logo.png";
+import ReutersLogo from "../../assets/images/reuters-logo.jpg";
+import TheWashingtonPostLogo from "../../assets/images/washington-post-logo.png";
+import NewsweekLogo from "../../assets/images/newsweek-logo.webp";
+import TimeLogo from "../../assets/images/time-logo.png";
+import APLogo from "../../assets/images/ap-logo.svg";
+
 export enum NewsCategoriesEnum {
   mostTrendingNews = "Most Trending News",
   politics = "Politics",
@@ -17,3 +29,34 @@ export enum NewsCategoriesEnum {
 export const newsCategories: NewsCategoriesEnum[] = Object.values(
   NewsCategoriesEnum
 ) as NewsCategoriesEnum[];
+
+export const getNewsLogo = (source: string | null | undefined) => {
+  switch (source) {
+    case "cbs-news":
+      return CBSNewsLogo;
+    case "ap":
+    case "associated-press":
+      return APLogo;
+    case "axios":
+      return AxiosLogo;
+    case "cnn":
+      return CNNLogo;
+    case "fox-news":
+      return FoxNewsLogo;
+    case "nbc-news":
+      return NBCNewsLogo;
+    case "usa-today":
+      return USATodayLogo;
+    case "reuters":
+      return ReutersLogo;
+    case "the-washington-post":
+      return TheWashingtonPostLogo;
+    case "newsweek":
+      return NewsweekLogo;
+    case "time":
+      return TimeLogo;
+
+    default:
+      return null;
+  }
+};
