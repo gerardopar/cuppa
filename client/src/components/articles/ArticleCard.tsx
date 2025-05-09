3764c51d1a7ad292acd3ad63a69d398e14cdbd16
Gerardo Paredes <gerardparedes23@gmail.com>
2025-05-09 00:12:09 -0700

    anchor img

--- client/src/components/articles/ArticleCard.tsx
+++ client/src/components/articles/ArticleCard.tsx
@@ -43,20 +43,24 @@ const ArticleCard: React.FC<{
   }
 
   return (
-    <a
-      href={article?.url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={`flex pr-8 w-[32.5%] items-center mr-2 mb-6 relative cursor-pointer ${className}`}
+    <div
+      className={`flex pr-8 w-[32.5%] items-center mr-2 mb-6 relative ${className}`}
     >
-      <div className="h-[100px] w-[100px] min-h-[100px] min-w-[100px] mr-4 shadow-sm rounded-[12px] overflow-hidden">
-        <img
-          className="w-full h-full object-cover transform transition-transform duration-300 ease-in-out hover:scale-105"
-          src={imgSrc ?? NewsEmptyPlaceholder}
-          alt="article img"
-          onError={() => setImgSrc(NewsEmptyPlaceholder)}
-        />
-      </div>
+      <a
+        href={article?.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative cursor-pointer"
+      >
+        <div className="h-[100px] w-[100px] min-h-[100px] min-w-[100px] mr-4 shadow-sm rounded-[12px] overflow-hidden">
+          <img
+            className="w-full h-full object-cover transform transition-transform duration-300 ease-in-out hover:scale-105"
+            src={imgSrc ?? NewsEmptyPlaceholder}
+            alt="article img"
+            onError={() => setImgSrc(NewsEmptyPlaceholder)}
+          />
+        </div>
+      </a>
       <div className="flex flex-col w-full">
         <p className="line-clamp-2 font-montserrat text-base font-medium">
           {article?.title}
@@ -76,7 +80,7 @@ const ArticleCard: React.FC<{
       <button type="button" className="absolute top-0 right-0 cursor-pointer">
         <MoreVertIcon className="text-gray-500" />
       </button>
-    </a>
+    </div>
   );
 };
 
--- client/src/components/articles/LargeArticleCard.tsx
+++ client/src/components/articles/LargeArticleCard.tsx
@@ -48,19 +48,23 @@ export const LargeArticleCard: React.FC<LargeArticleCardProps> = ({
   }
 
   return (
-    <a
-      href={article?.url}
-      target="_blank"
-      rel="noopener noreferrer"
+    <div
       className={`group flex-1 flex items-end justify-start h-full relative mr-4 p-4 rounded-[12px] cursor-pointer overflow-hidden ${className}`}
     >
-      <img
-        src={article?.urlToImage ?? ""}
-        alt=""
-        className="hidden"
-        onLoad={() => setBgUrl(article?.urlToImage ?? "")}
-        onError={() => setBgUrl(NewsEmptyPlaceholder)}
-      />
+      <a
+        href={article?.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="h-full relative rounded-[12px] cursor-pointer overflow-hidden"
+      >
+        <img
+          src={article?.urlToImage ?? ""}
+          alt=""
+          className="hidden"
+          onLoad={() => setBgUrl(article?.urlToImage ?? "")}
+          onError={() => setBgUrl(NewsEmptyPlaceholder)}
+        />
+      </a>
 
       <div
         className="absolute inset-0 bg-center bg-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
@@ -81,7 +85,7 @@ export const LargeArticleCard: React.FC<LargeArticleCardProps> = ({
           />
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
