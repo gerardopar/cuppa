df0d325ac2eb19911c3e7e4cf6deb13226a10a4a
Gerardo Paredes <gerardparedes23@gmail.com>
2025-05-11 22:02:17 -0700

    update SmallArticleCardVertical, add skeleton laoder

--- client/src/components/articles/SmallArticleCardVertical.tsx
+++ client/src/components/articles/SmallArticleCardVertical.tsx
@@ -5,11 +5,13 @@ import PublishedDate from "../shared/PublishedDate";
 import NewsEmptyPlaceholder from "../../assets/images/news-empty-placeholder.jpg";
 
 import { Article } from "../../types/article";
+import { Skeleton } from "@mui/material";
 
-export const SmallArticleCard: React.FC<{
+export const SmallArticleCardVertical: React.FC<{
   article: Article;
   containerClassName?: string;
-}> = ({ article, containerClassName = "" }) => {
+  loading?: boolean;
+}> = ({ article, containerClassName = "", loading = false }) => {
   const [bgUrl, setBgUrl] = useState<string>(
     article?.urlToImage ?? NewsEmptyPlaceholder
   );
@@ -17,6 +19,32 @@ export const SmallArticleCard: React.FC<{
   useEffect(() => {
     setBgUrl(article?.urlToImage ?? NewsEmptyPlaceholder);
   }, [article?.urlToImage]);
+
+  if (loading || !article) {
+    return (
+      <div
+        className={`flex-1 flex flex-col items-end justify-start h-full bg-center bg-cover p-4 rounded-[12px] relative ${containerClassName}`}
+      >
+        <div className="w-full h-40 relative rounded-[12px] overflow-hidden">
+          <Skeleton variant="rectangular" width="100%" height="100%" />
+          <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent z-10 rounded-[12px]" />
+        </div>
+
+        <div className="flex flex-col z-10 mt-4 w-full">
+          <Skeleton variant="text" width="80%" height={24} />
+          <Skeleton variant="text" width="60%" height={24} className="mt-1" />
+
+          <div className="flex items-center justify-between mt-3">
+            <div className="flex items-center space-x-2">
+              <Skeleton variant="circular" width={32} height={32} />
+              <Skeleton variant="text" width={80} height={20} />
+            </div>
+            <Skeleton variant="text" width={60} height={20} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div
@@ -58,4 +86,4 @@ export const SmallArticleCard: React.FC<{
   );
 };
 
-export default SmallArticleCard;
+export default SmallArticleCardVertical;
