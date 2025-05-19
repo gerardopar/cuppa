d8a65e6f3f2b50de84545f97a57fc94840e09dda
Gerardo Paredes <gerardparedes23@gmail.com>
2025-05-18 23:52:09 -0700

    add quoteOfTheDay

---
+++ client/src/components/quote-of-the-day/QuoteOfTheDay.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+import { useGetPoliticalQuoteWithImage } from "../../react-query/queries/openAi";
+import { GavelOutlined } from "@mui/icons-material";
+import { Skeleton } from "@mui/material";
+
+export const QuoteOfTheDay: React.FC<{ containerClassName?: string }> = ({
+  containerClassName,
+}) => {
+  const {
+    data: quoteWithImage,
+    isLoading,
+    isFetching,
+  } = useGetPoliticalQuoteWithImage();
+
+  if (isLoading || isFetching) {
+    return (
+      <div
+        className={`w-full flex flex-col items-center justify-between bg-[var(--primary-dark)]/90 p-4 rounded-[20px] ${containerClassName}`}
+      >
+        <div className="w-full flex items-center justify-start">
+          <div className="flex items-center justify-center rounded-[20px] h-[40px] w-[40px] overflow-hidden">
+            <Skeleton
+              variant="circular"
+              width={40}
+              height={40}
+              sx={{ bgcolor: "grey.800" }}
+            />
+          </div>
+          <div className="flex items-center justify-center rounded-full h-[40px] w-[40px] overflow-hidden ml-[-12px]">
+            <Skeleton
+              variant="circular"
+              width={40}
+              height={40}
+              sx={{ bgcolor: "grey.700" }}
+              className="z-10"
+            />
+          </div>
+        </div>
+
+        <div className="w-full flex items-center justify-start mt-4">
+          <Skeleton
+            variant="text"
+            width="80%"
+            height={20}
+            sx={{ bgcolor: "grey.800" }}
+          />
+        </div>
+
+        <div className="w-full flex items-center justify-start mt-4">
+          <Skeleton
+            variant="text"
+            width="40%"
+            height={16}
+            sx={{ bgcolor: "grey.700" }}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-between bg-[var(--primary-dark)]/90 p-4 rounded-[20px] ${containerClassName}`}
+    >
+      <div className="w-full flex items-center justify-start">
+        <div className="flex items-center justify-center rounded-[20px] h-[40px] w-[40px] overflow-hidden border-solid border-[1px] border-white">
+          <GavelOutlined className="text-white" />
+        </div>
+        <div className="flex items-center justify-center rounded-full h-[40px] w-[40px] overflow-hidden ml-[-12px] border-solid border-[1px] border-white">
+          <img src={quoteWithImage?.image} alt="" />
+        </div>
+      </div>
+
+      <div className="w-full flex items-center justify-start mt-4">
+        <p className="text-[14px] font-bold text-white">
+          {quoteWithImage?.quote}
+        </p>
+      </div>
+
+      <div className="w-full flex items-center justify-start mt-4">
+        <p className="text-xs font-bold text-gray-100">
+          {quoteWithImage?.author}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteOfTheDay;
--- client/src/pages/politics/PoliticsPage.tsx
+++ client/src/pages/politics/PoliticsPage.tsx
@@ -3,17 +3,17 @@ import React, { useMemo } from "react";
 import TopBar from "../../components/top-bar/TopBar";
 import ArticleCard from "../../components/articles/ArticleCard";
 import LargeArticleCard from "../../components/articles/LargeArticleCard";
+import QuoteOfTheDay from "../../components/quote-of-the-day/QuoteOfTheDay";
 import NewsCategoryButtonList from "../../components/new-categories/NewsCategoryButtonList";
 import PaginatedArticlesList from "../../components/articles/paginatedArticles/PaginatedArticlesList";
 
 import { usePaginatedNews } from "../../react-query/queries/news";
-import { useGetPoliticalQuoteWithImage } from "../../react-query/queries/openAi";
+import { useGetPolls } from "../../react-query/queries/polls";
 
 import {
   NewsCategoriesEnum,
   dedupeArticles,
 } from "../../react-query/helpers/news.helpers";
-import { useGetPolls } from "../../react-query/queries/polls";
 
 export const PoliticsPage: React.FC = () => {
   const {
@@ -33,10 +33,7 @@ export const PoliticsPage: React.FC = () => {
     poll_type: "approval",
   });
 
-  const { data: quoteWithImage } = useGetPoliticalQuoteWithImage();
-
-  console.log(quoteWithImage);
-  console.log(polls);
+  console.log("polls", polls);
 
   const rawArticles = useMemo(
     () => paginatedNews?.pages.flatMap((page) => page.articles) ?? [],
@@ -60,22 +57,29 @@ export const PoliticsPage: React.FC = () => {
         </div>
 
         <div className="flex flex-col w-full mt-4">
-          <div className="w-full h-[400px] flex items-center justify-center max-h-[400px] p-4 border-solid border-[1px] border-gray-100 rounded-[20px]">
-            <div className="w-[50%] h-full">
-              <LargeArticleCard article={articles[0]} loading={isLoading} />
+          <div className="w-full h-[400px] flex items-center justify-center max-h-[400px]">
+            <div className="w-[80%] h-full flex items-center justify-center p-6 border-solid border-[1px] border-gray-100 rounded-[20px] mr-4">
+              <div className="w-[50%] h-full">
+                <LargeArticleCard article={articles[0]} loading={isLoading} />
+              </div>
+
+              <div className="w-[50%] h-full flex flex-col items-center justify-between">
+                {articles?.slice(1, 4)?.map((a, i) => {
+                  return (
+                    <ArticleCard
+                      key={a?.url ?? i}
+                      article={a}
+                      className="w-full !m-0"
+                      loading={isLoading}
+                    />
+                  );
+                })}
+              </div>
             </div>
 
-            <div className="w-[50%] h-full flex flex-col items-center justify-between">
-              {articles?.slice(1, 4)?.map((a, i) => {
-                return (
-                  <ArticleCard
-                    key={a?.url ?? i}
-                    article={a}
-                    className="w-full !m-0"
-                    loading={isLoading}
-                  />
-                );
-              })}
+            <div className="w-[20%] h-full flex flex-col items-center justify-between p-4 border-solid border-[1px] border-gray-100 rounded-[20px]">
+              <QuoteOfTheDay containerClassName="mb-2 flex-1" />
+              <QuoteOfTheDay containerClassName="flex-1" />
             </div>
           </div>
 
