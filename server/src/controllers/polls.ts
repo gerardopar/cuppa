84e60fb1e796e96daaf3d9e9a39ed1e20af4e907
Gerardo Paredes <gerardparedes23@gmail.com>
2025-05-20 23:20:40 -0700

    cache latest polls

--- server/src/controllers/polls.ts
+++ server/src/controllers/polls.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
+import redisClient from "../cache/redis/redisClient";
+
+import { PollResponse } from "../types/voteHubApi";
 import { getPollsData } from "../services/voteHubApi-service";
+
 import { PollType, PopulationType } from "../types/voteHubApi";
 
 export const getPolls = async (req: Request, res: Response): Promise<void> => {
   try {
+    const cacheKey = "polls:latest";
+
     const {
       poll_type = "",
       pollster = "",
@@ -14,24 +20,33 @@ export const getPolls = async (req: Request, res: Response): Promise<void> => {
       population = "",
     } = req.query;
 
-    const polls = await getPollsData({
-      poll_type: poll_type as PollType,
-      pollster: pollster as string,
-      subject: subject as string,
-      from_date: from_date as string,
-      to_date: to_date as string,
-      min_sample_size: parseInt(min_sample_size as string),
-      population: population as PopulationType,
-    });
+    const cached = await redisClient.get(cacheKey);
+    if (cached) {
+      const polls = JSON.parse(cached) as PollResponse[];
+      res.json(polls);
+      return;
+    } else {
+      const polls = await getPollsData({
+        poll_type: poll_type as PollType,
+        pollster: pollster as string,
+        subject: subject as string,
+        from_date: from_date as string,
+        to_date: to_date as string,
+        min_sample_size: parseInt(min_sample_size as string),
+        population: population as PopulationType,
+      });
 
-    const sortedPolls = polls.sort(
-      (a, b) =>
-        new Date(b.start_date).getTime() - new Date(a.start_date).getTime()
-    );
+      const sortedPolls = polls.sort(
+        (a, b) =>
+          new Date(b.start_date).getTime() - new Date(a.start_date).getTime()
+      );
 
-    const limitedPolls = sortedPolls.slice(0, 20);
+      const limitedPolls = sortedPolls.slice(0, 20);
 
-    res.json(limitedPolls);
+      await redisClient.setEx(cacheKey, 86400, JSON.stringify(limitedPolls));
+
+      res.json(limitedPolls);
+    }
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch polls" });
   }
